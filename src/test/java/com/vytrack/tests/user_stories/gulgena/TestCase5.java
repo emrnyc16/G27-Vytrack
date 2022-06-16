9c6edcb5663e9439d76c6977e263fdf6e89fc7b5
Gulgena <gistyakova@gmail.com>
2022-06-16 16:57:59 -0400

    commit

---
+++ src/test/java/com/vytrack/tests/user_stories/gulgena/TestCase5.java
@@ -0,0 +1,31 @@
+package com.vytrack.tests.user_stories.gulgena;
+
+public class TestCase5 {
+
+}
+
+//Truck driver see all cars' cost informations on the Vehicle Costs page
+
+
+
+ //  Given I am on qa2.vytrack Login page
+
+
+
+//     Then i click on Fleet menu	When I enter Truck Drivers user name
+
+
+
+
+//   And I enter Truck Drivers password
+
+
+// And I click on Login Button
+
+// And i click on Fleet menu
+//  And select Vehicle cost from the list
+//And click "Create Vehicle Costs" button
+//I fill out all fields with valid data
+//  And click save and close button
+// Then i should see create Vehicle cost notification
+
