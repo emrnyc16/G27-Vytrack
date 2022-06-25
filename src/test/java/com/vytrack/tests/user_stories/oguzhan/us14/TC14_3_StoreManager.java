44e61dd64a84e35109b302253c88df95cc3195f9
Oguzhan87 <aydogan.oguzhan@hotmail.com>
2022-06-24 23:31:58 -0400

    Oguzhan TC14.3 Updated

--- src/test/java/com/vytrack/tests/user_stories/oguzhan/us14/TC14_3_StoreManager.java
+++ src/test/java/com/vytrack/tests/user_stories/oguzhan/us14/TC14_3_StoreManager.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class TC14_3_StoreManager extends TestBase {
 
@@ -26,16 +27,20 @@ public class TC14_3_StoreManager extends TestBase {
         WebElement createVehicleModel = driver.findElement(By.xpath("//a[@title='Create Vehicles Model']"));
         createVehicleModel.click();
 
-        WebElement fuelType = driver.findElement(By.xpath("(//ul[@class='select2-results'])[2]"));
-        fuelType.isEnabled();
+        WebElement fuelType = driver.findElement(By.xpath("(//span[@class='select2-chosen'])[2]"));
+        fuelType.click();
 
-       Thread.sleep(3000);
+        List<WebElement> fuelTypeList = driver.findElements(By.className("select2-result-label"));
+        for (WebElement each : fuelTypeList) {
+            System.out.println("each.getText() = " + each.getText() + " is displayed: " + each.isDisplayed());
 
-        File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(source, new File("C:\\Users\\aydog\\Downloads\\VyTrack Store Manager\\ScreenShot.png"));
+            Thread.sleep(3000);
+
+            File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(source, new File("C:\\Users\\aydog\\Downloads\\VyTrack Store Manager\\ScreenShot.png"));
 
 
+        }
 
     }
-
 }
